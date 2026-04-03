902c792ef46da063b57714426eeeb6ebefcc5303
RealRafaqatAli <rafaqat.ali.37301@gmail.com>
2026-04-03 10:45:31 +0500

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export default function Header() {
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "gOne"
+  );
+
+  useEffect(() => {
+    document.documentElement.className = "";
+    document.documentElement.classList.add(theme);
+
+    // ✅ save theme
+    localStorage.setItem("theme", theme);
+
+  }, [theme]);
+
+  return (
+    <header>
+      <div className="logo">
+        <span>Taskapp</span>
+      </div>
+
+      <div className="themeSelector">
+        <span className={theme==="light"?"light activeTheme":"light"} onClick={()=>setTheme("light")}></span>
+
+        <span className={theme==="medium"?"medium activeTheme":"medium"} onClick={()=>setTheme("medium")}></span>
+
+        <span className={theme==="dark"?"dark activeTheme":"dark"} onClick={()=>setTheme("dark")}></span>
+
+        <span className={theme==="gOne"?"gOne activeTheme":"gOne"} onClick={()=>setTheme("gOne")}></span>
+
+        <span className={theme==="gTwo"?"gTwo activeTheme":"gTwo"} onClick={()=>setTheme("gTwo")}></span>
+
+        <span className={theme==="gThree"?"gThree activeTheme":"gThree"} onClick={()=>setTheme("gThree")}></span>
+      </div>
+    </header>
+  );
+}
